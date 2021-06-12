63df1c5adc6d9eef8a3a310a9580b8980ab5819a
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-12 12:11:03 -0700

    change mongoose connect

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget-tracker', {
+mongoose.connect(MONGODB_URI || "mongodb://localhost/budget-tracker", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useFindAndModify: false
